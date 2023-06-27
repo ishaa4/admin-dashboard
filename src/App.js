0a8f38aa8db1dcef789fa9af645004c02ef7ecf0
Isha <ishakatyayani4@gmail.com>
2023-06-27 16:14:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import LoginPage from './LoginPage';
+import SignupPage from './SignupPage';
+import SignupForm from './SigupForm';
+// const arr = [{
+//   email: 'isha@gmail.com',
+//   password: 'password',
+//   isAdmin: true
+// }
+// ]
+const App = () => {
+  const [userDatabase, setUserDatabase] = useState([]);
+  const [isLoginPage, setIsLoginPage] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <Router>
+    //   <div>
+    //     <nav>
+    //       <ul>
+    //         <li>
+    //           <Link to="/login">Login</Link>
+    //         </li>
+    //         <li>
+    //           <Link to="/signup">Signup</Link>
+    //         </li>
+    //       </ul>
+    //     </nav>
+    //     <Route exact path="/login" component={LoginPage} />
+    //     <Route exact path="/signup" component={SignupPage} />
+    //   </div>
+    // </Router>
+
+    <div>
+      <SignupForm isLoginPage={isLoginPage} setIsLoginPage={setIsLoginPage}/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/SigupForm.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const SignupForm = ({isLoginPage, setIsLoginPage}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isAdminValue, setIsAdminValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Signing up...', email, password, isAdminValue);
+  };
+
+  const formHandler = () => {
+    setIsLoginPage(!isLoginPage)
+  }
+
+  return (
+    <form style={{display: 'flex', flexDirection: 'column', maxWidth: '400px'}} onSubmit={handleSubmit}>
+      <h2>{isLoginPage ? 'Login' : 'Signup'}</h2>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      { !isLoginPage ? <div style={{display: 'flex'}}>
+        <label for="admin">Are you an Admin ?</label>
+        <input onChange={(e) => setIsAdminValue(e.target.checked)} type="checkbox"/>
+      </div>: null}
+      <button type="submit">{isLoginPage ? 'Login' : 'Signup'}</button>
+      <p>{isLoginPage ? 'New here ?' : 'Already a member ?'} <span onClick={formHandler} style={{color: 'orange', cursor: 'pointer'}}>{isLoginPage ? 'Sign Up' : 'Sign In'}</span></p>
+    </form>
+  );
+};
+
+export default SignupForm;
